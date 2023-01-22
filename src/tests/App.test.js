6d0ecd499c10402d42edf1df2079d45bdad90f5c
Joey Hetfield <joeydefenix@hotmail.com>
2023-01-22 19:06:51 -0300

    Dia todo sem nenhum sucesso, feito o começo de testes pelo menos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,79 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('../services/FetchApi', () => {
+  return jest.fn(() => 'mocked function')
+})
+
+describe('Testar A aplicação', () => {
+  test('Testar se os inputs existem', () => {
+    render(<App />);
+      const textBox = screen.getByRole('textbox');
+      expect(textBox).toBeInTheDocument();
+
+      const numberInput = screen.getByRole('spinbutton');
+      expect(numberInput).toBeInTheDocument();
+  });
+  test('Testar se o botão existe', () => {
+    render(<App />);
+    const btn = screen.getByTestId('button-filter');
+    expect(btn).toBeInTheDocument();  
+  })
+  test('Se todas categorias existem: ', () => {
+    render(<App />);
+    const name = screen.getByRole('columnheader', {
+      name: /name/i
+    });
+    expect(name).toBeInTheDocument();
+
+
+    const rotation = screen.getByRole('columnheader', {
+      name: /rotation period/i
+    });
+    expect(rotation).toBeInTheDocument();
+
+    const orbital = screen.getByRole('columnheader', {
+      name: /orbital period/i
+    });
+    expect(orbital).toBeInTheDocument();
+
+    const diameter = screen.getByRole('columnheader', {
+      name: /diameter/i
+    });
+    expect(diameter).toBeInTheDocument();
+
+    const climate = screen.getByRole('columnheader', {
+      name: /climate/i
+    });
+    expect(climate).toBeInTheDocument();
+
+    const gravity = screen.getByRole('columnheader', {
+      name: /gravity/i
+    });
+    expect(gravity).toBeInTheDocument();
+
+    const terrain = screen.getByRole('columnheader', {
+      name: /terrain/i
+    });
+    expect(terrain).toBeInTheDocument();
+
+    const surface = screen.getByRole('columnheader', {
+      name: /surface water/i
+    });
+
+    expect(surface).toBeInTheDocument();
+  })
+  // test('Se o filtro por nome funciona', () => {
+  //   render(<App />);
+
+  //   const textBox = screen.getByRole('textbox');
+  //   userEvent.type(textBox, 'Ta');
+
+
+  //   const tatooineElements = screen.queryAllByText(/tatooine/i);
+  //   expect(tatooineElements).toHaveLength(2);
+    
+  // })
+
+})
